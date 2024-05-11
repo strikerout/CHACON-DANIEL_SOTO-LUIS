801cba12277f4f822ff181d5798e835e441a85f7
Daniel Chacon <dani3l.uebp@gmail.com>
2024-05-11 16:43:50 -0300

    adding log4j properties

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import org.apache.log4j.Logger;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
@@ -8,7 +11,7 @@ public class Main {
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
+            logger.error("Esta es la iteración " + i, null);
             // Press Mayús+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
