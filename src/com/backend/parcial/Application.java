b87c0d85a9c01ca9dfee03ca52f7ab136033b61f
Daniel Chacon <dani3l.uebp@gmail.com>
2024-05-16 20:34:06 -0300

    doing some clean

--- src/com/backend/parcial/Application.java
+++ src/com/backend/parcial/Application.java
@@ -1,9 +1,12 @@
 package com.backend.parcial;
 
 import com.backend.parcial.repository.dbconnection.H2Connection;
+import org.apache.log4j.Logger;
 
 public class Application {
+    private static final Logger logger = Logger.getLogger(Application.class);
     public static void main(String[] args) {
         H2Connection.createTable();
+        logger.info("Application works :)");
     }
 }
