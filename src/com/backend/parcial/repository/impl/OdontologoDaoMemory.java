407181df2c910ac44fa3c6bcdb959ecb698ae083
luisesotor <luis13826@gmail.com>
2024-05-16 22:16:41 -0300

    Adding Odontologo DaoMemory

--- src/com/backend/parcial/repository/impl/OdontologoDaoMemory.java
+++ src/com/backend/parcial/repository/impl/OdontologoDaoMemory.java
@@ -3,16 +3,35 @@ package com.backend.parcial.repository.impl;
 import com.backend.parcial.entity.Odontologo;
 import com.backend.parcial.repository.IDao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoMemory implements IDao<Odontologo> {
+
+    List<Odontologo> listaOdontologos = new ArrayList<>();
+
+    public OdontologoDaoMemory() {
+        Odontologo odontologo1 = new Odontologo( 1L,123654L ,"Pedro", "Estocolmo" );
+        Odontologo odontologo2 = new Odontologo( 2L,124654L ,"Maria", "Barcelona" );
+        Odontologo odontologo3 = new Odontologo( 3L,125654L ,"Fernanda", "Paris" );
+        Odontologo odontologo4 = new Odontologo( 4L,126654L ,"Diego", "Monterrey" );
+
+        listaOdontologos.add(odontologo1);
+        listaOdontologos.add(odontologo2);
+        listaOdontologos.add(odontologo3);
+        listaOdontologos.add(odontologo4);
+    }
+
+
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        return null;
+        Long id = Long.valueOf (listaOdontologos.size()+1);
+        Odontologo odontologoAGuardar = new Odontologo(id, odontologo.getNumMatricula(), odontologo.getNombre(), odontologo.getApellido());
+        return odontologoAGuardar;
     }
 
     @Override
     public List<Odontologo> listarTodos() {
-        return null;
+        return listaOdontologos;
     }
 }
