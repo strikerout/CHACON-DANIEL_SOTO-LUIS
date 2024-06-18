2f95f2d05f23d702a9e4695caf744c62cd78dcfe
Daniel Chacon <dani3l.uebp@gmail.com>
2024-06-17 21:28:42 -0300

    fixing odontologo and turn

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,9 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function mostrarSeccion(seccionId, claseSeccion) {
-    // Ocultar todas las secciones específicas de la página
-    document.getElementById('modificar-paciente').style.display = 'none';
-    document.getElementById('resultado-busqueda').style.display = 'none';
+    // Ocultar todas las secciones específicas de la página, si existen
+    var modificarPaciente = document.getElementById('modificar-paciente');
+    if (modificarPaciente) {
+        modificarPaciente.style.display = 'none';
+    }
+    
+    var resultadoBusqueda = document.getElementById('resultado-busqueda');
+    if (resultadoBusqueda) {
+        resultadoBusqueda.style.display = 'none';
+    }
+
     var secciones = document.querySelectorAll(claseSeccion);
     secciones.forEach(function (seccion) {
         seccion.classList.remove('mostrar');
